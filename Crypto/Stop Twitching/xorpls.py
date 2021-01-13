8bd54139e9b9b2ef2f471a1e1515bfbdd396a411
dootdoot1111 <53709349+dootdoot1111@users.noreply.github.com>
2021-01-13 14:16:05 +0530

    Update xorpls.py

--- Crypto/Stop Twitching/xorpls.py
+++ Crypto/Stop Twitching/xorpls.py
@@ -16,13 +16,22 @@ for x in range(0, c):
 
 intArr = []
 for x in range(1, len(ascii_lowercase)):
-    intArr.append(bin(x)[2:].zfill(5))        # just created an array having binary numbers from 1 to 26
+    intArr.append(bin(x)[2:].zfill(5))        # just created an array having binary numbers from 1 to 26 
 
-bacon_dict = dict(zip(intArr, ascii_lowercase))     # bacon cipher encodes all alphabets as binary numbers of the position of the alphabet. With a having position 0 to z having position 25
+bacon_dict = dict(zip(intArr, ascii_lowercase))     
 
-print("BITSCTF{", end='')       # just ensuring the flag format. The end='; stuff just ensured we dont print a newline
+# bacon cipher encodes all alphabets as binary numbers of the position of the alphabet. With a having position 0 to z having position 25
+
+# In the intArr we created, we had the array starting at 1 and ending at 26, but the bacon cipher encodes a to 0 and z to 25 right? 
+# Well, yes, but if you notice the key and its binary representaion in the challenge text, you'll see the that the binary representation
+# when decoded as bacon cipher actually gives the string key, but the alphabets are shifted by 1. So instead, we just mapped the positions 
+# of the alphabets to its next position, or we mapped a to 1 and z to 26, since its the same as shifting the 0th alphabet to the 1st one.
+# I tried to explain. Emphasis on tried.
+
+print("BITSCTF{", end='')       # just ensuring the flag format. The end=''; stuff just ensured we don't print a newline
 
 for x in range(0, len(bin_key), 5):
-    print(bacon_dict[bin_key[x:x + 5]], end='')     # reads the binary form of the decimal key, 5 bits at a time, looks for the corresponding alphabet and prints it
+    print(bacon_dict[bin_key[x:x + 5]], end='')     
+    # reads the binary form of the decimal key, 5 bits at a time, looks for the corresponding alphabet for the bacon cipher and prints it
 
 print("}")              # flag format. can't score with it, can't score without it.
